0bf34f7da6ca29335cf458367d8bbe1988d2fad8
SHENZA <shenza@oocl.com>
2024-11-21 14:24:29 +0800

    [oo]test:test step 5 should_return_true_when_isLeader_given_student_is_the_leader

--- src/test/java/oo/step5/KlassTest.java
+++ src/test/java/oo/step5/KlassTest.java
@@ -37,18 +37,18 @@ public class KlassTest {
 
         assertThat(isLeader).isFalse();
     }
-//
-//    @Test
-//    public void should_return_true_when_isLeader_given_student_is_the_leader() {
-//        Student tom = new Student(1, "Tom", 21);
-//        Klass klass = new Klass(1);
-//        tom.join(klass);
-//        klass.assignLeader(tom);
-//
-//        boolean isLeader = klass.isLeader(tom);
-//
-//        assertThat(isLeader).isTrue();
-//    }
+
+    @Test
+    public void should_return_true_when_isLeader_given_student_is_the_leader() {
+        Student tom = new Student(1, "Tom", 21);
+        Klass klass = new Klass(1);
+        tom.join(klass);
+        klass.assignLeader(tom);
+
+        boolean isLeader = klass.isLeader(tom);
+
+        assertThat(isLeader).isTrue();
+    }
 //
 //    @Test
 //    public void should_print_prompt_message_when_assign_leader_given_student_is_not_in_class() throws Exception {
