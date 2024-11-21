30c0898079f65c2ae5b286a127c6dbf436d65564
SHENZA <shenza@oocl.com>
2024-11-21 23:45:11 +0800

    [oo]feat:update step 7

--- src/main/java/oo/School.java
+++ src/main/java/oo/School.java
@@ -1,53 +1,56 @@
 package oo;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class School {
+    private int id;
     private String name;
-    private Set<Klass> classes;
-    private Set<Teacher> teachers;
+    private List<Teacher> teachers;
+    private List<Student> students;
 
-    public School(String name) {
+    public School() {
+    }
+
+    public School(int id, String name, List<Teacher> teachers, List<Student> students) {
+        this.id = id;
         this.name = name;
-        this.classes = new HashSet<>();
-        this.teachers = new HashSet<>();
+        this.teachers = teachers;
+        this.students = students;
     }
 
-    // 添加班级
-    public void addKlass(Klass klass) {
-        classes.add(klass);
+    private String generateInfo(List<? extends Person> people) {
+        return (people == null || people.isEmpty()) ?
+                "empty" :
+                people.stream()
+                        .map(person -> {
+                            String original = person.introduce();
+                            return (original.endsWith(".") ? original.substring(0, original.length() - 1) : original) +
+                                    " in " + name + " School.";
+                        })
+                        .collect(Collectors.joining("\n"));
     }
 
-    // 添加教师
-    public void addTeacher(Teacher teacher) {
-        teachers.add(teacher);
-        teacher.introduceWords = teacher.introduceWords.replace("I am a teacher.", "I am a teacher in Greenwood School.");
+    public String startSchool() {
 
+        String teachersInfo = generateInfo(teachers);
+        String studentsInfo = generateInfo(students);
+
+        return "Our teachers:\n" + teachersInfo + "\nOur students:\n" + studentsInfo;
     }
 
-    // 获取班级
-    public Klass getKlass(int number) {
-        for (Klass klass : classes) {
-            if (klass.getNumber() == number) {
-                return klass;
-            }
-        }
-        return null;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        School school = (School) o;
+        return id == school.id;
     }
 
-    // 获取所有班级
-    public Set<Klass> getClasses() {
-        return classes;
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
-    // 获取所有教师
-    public Set<Teacher> getTeachers() {
-        return teachers;
-    }
-
-    // 学校的名称
-    public String getName() {
-        return name;
-    }
 }
--- src/test/java/oo/step7/SchoolTest.java
+++ src/test/java/oo/step7/SchoolTest.java
@@ -1,14 +1,14 @@
 package oo.step7;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
-import oo.Klass;
-import oo.School;
-import oo.Student;
-import oo.Teacher;
+
+import oo.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,17 +22,111 @@ public class SchoolTest {
     }
 
     @Test
-    public void should_print_message_when_teacher_is_assigned_to_school() {
-        // Given
-        School school = new School("Greenwood School");
-        Teacher jerry = new Teacher(1, "Jerry", 21);
+    public void should_return_true_when_equals_given_two_school_with_same_id() {
+        School school1 = new School(1,"A",null, null);
+        School school2 = new School(1,"B",null, null);
 
+        assertThat(school1).isEqualTo(school2);
+    }
+
+    @Test
+    public void should_no_teacher_and_no_student_introduce_when_null_school_startSchool_() {
+        // Given
+        School school = new School(1,"A",null, null);
         // When
-        school.addTeacher(jerry);
-        String introduce = jerry.introduce();
+        String result = school.startSchool();
 
         // Then
-        assertThat(introduce).contains("My name is Jerry. I am 21 years old. I am a teacher in Greenwood School.");
+        String expectedOutput = "Our teachers:\n" +
+                "empty\n" +
+                "Our students:\n" +
+                "empty";
+
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void should_has_teacher_and_no_student_introduce_when_null_school_startSchool_() {
+        // Given
+        Teacher jerry = new Teacher(1, "Jerry", 35);
+        Klass klass1 = new Klass(1);
+        jerry.assignTo(klass1);
+        List<Teacher> teachers = new ArrayList<>();
+        teachers.add(jerry);
+        School school = new School(1,"A",teachers, null);
+        // When
+        String result = school.startSchool();
+
+        // Then
+        String expectedOutput = "Our teachers:\n" +
+                "My name is Jerry. I am 35 years old. I am a teacher. I teach Class 1 in A School.\n" +
+                "Our students:\n" +
+                "empty";
+
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void should_no_teacher_and_has_student_introduce_when_null_school_startSchool_() {
+        // Given
+        Student tom = new Student(1, "Tom", 18);
+        Klass klass1 = new Klass(1);
+        tom.join(klass1);
+        klass1.assignLeader(tom);
+
+        Student zane = new Student(2, "Zane", 18);
+        Klass klass2 = new Klass(2);
+        tom.join(klass2);
+
+        List<Student> students = new ArrayList<>();
+        students.add(tom);
+        students.add(zane);
+        School school = new School(1,"A",null, students);
+        // When
+        String result = school.startSchool();
+
+        // Then
+        String expectedOutput = "Our teachers:\n" +
+                "empty\n" +
+                "Our students:\n" +
+                "My name is Tom. I am 18 years old. I am a student. I am in class 2 in A School.\n" +
+                "My name is Zane. I am 18 years old. I am a student in A School.";
+
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void should_all_teacher_and_student_introduce_when_startSchool_() {
+        // Given
+        Student tom = new Student(1, "Tom", 18);
+        Klass klass1 = new Klass(1);
+        tom.join(klass1);
+        klass1.assignLeader(tom);
+
+        Student zane = new Student(2, "Zane", 18);
+        Klass klass2 = new Klass(2);
+        tom.join(klass2);
+
+        Teacher jerry = new Teacher(1, "Jerry", 35);
+        jerry.assignTo(klass1);
+        jerry.assignTo(klass2);
+        List<Student> students = new ArrayList<>();
+        students.add(tom);
+        students.add(zane);
+        List<Teacher> teachers = new ArrayList<>();
+        teachers.add(jerry);
+        School school = new School(1,"A",teachers, students);
+        // When
+        String result = school.startSchool();
+
+        // Then
+        String expectedOutput = "Our teachers:\n" +
+                "My name is Jerry. I am 35 years old. I am a teacher. I teach Class 1, 2 in A School.\n" +
+                "Our students:\n" +
+                "My name is Tom. I am 18 years old. I am a student. I am in class 2 in A School.\n" +
+                "My name is Zane. I am 18 years old. I am a student in A School.";
+
+        assertEquals(expectedOutput, result);
     }
 
     private String systemOut() {
