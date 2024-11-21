ef5f5eef2985b12d5e4402c6af8fe433e9024165
SHENZA <shenza@oocl.com>
2024-11-21 13:42:33 +0800

    [oo]feat: step2

--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,4 +1,13 @@
 package oo;
 
-public class Student {
+public class Student extends Person {
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a student.";
+    }
 }
--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,4 +1,12 @@
 package oo;
 
-public class Teacher {
+public class Teacher extends Person {
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a teacher.";
+    }
 }
